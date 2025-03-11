7383f9c041657c4bcb71471c04aca9db92aab939
shauncoding28 <196330731+shauncoding28@users.noreply.github.com.>
2025-03-11 22:22:18 +0100

    install valid-url to validate input, create database to store urls, add shortening and re-direct functionality, test and add comments

--- index.js
+++ index.js
@@ -2,23 +2,70 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const validUrl = require('valid-url')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const urlPattern = /^(https?:\/\/)(www\.)?[a-z0-9\-]+\.[a-z]{2,}([\/\w\.-]*)*\/?$/i;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+let urlCount = 1; // Use let to allow incrementing
+const urlDatabase = {};
+
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  if (!originalUrl || !validUrl.isWebUri(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const shortUrl = urlCount++;
+  urlDatabase[shortUrl] = originalUrl;
+
+  res.status(200).json({ original_url: originalUrl, short_url: shortUrl });
+});
+
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  try {
+    new URL(originalUrl);
+  } catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const shortUrl = urlCount++;
+  urlDatabase[shortUrl] = originalUrl;
+
+  res.status(200).json({ original_url: originalUrl, short_url: shortUrl });
+});
+
+
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const urlId = req.params.shortUrl;
+  const originalUrl = urlDatabase[urlId];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).send('No url found');
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
